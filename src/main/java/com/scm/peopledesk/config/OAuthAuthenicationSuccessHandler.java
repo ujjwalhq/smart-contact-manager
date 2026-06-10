0ef6eba59600b54279b3b1e4ea2e119c010dac22
Ujjwal Singh <mrujjwal03@gmail.com>
2026-06-11 00:26:49 +0530

    feat: save Google OAuth2 user profile data to database after login

--- src/main/java/com/scm/peopledesk/config/OAuthAuthenicationSuccessHandler.java
+++ src/main/java/com/scm/peopledesk/config/OAuthAuthenicationSuccessHandler.java
@@ -1,14 +1,23 @@
 package com.scm.peopledesk.config;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import com.scm.peopledesk.entities.Providers;
+import com.scm.peopledesk.entities.User;
+import com.scm.peopledesk.helpers.AppConstants;
+import com.scm.peopledesk.repsitories.UserRepo;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,33 +25,57 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class OAuthAuthenicationSuccessHandler implements AuthenticationSuccessHandler {
 
-    Logger logger = LoggerFactory.getLogger(OAuthAuthenicationSuccessHandler.class);
+  Logger logger = LoggerFactory.getLogger(OAuthAuthenicationSuccessHandler.class);
+
+  @Autowired
+  private UserRepo userRepo;
 
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
       Authentication authentication) throws IOException, ServletException {
 
-        System.out.println("HANDLER EXECUTED");
+    System.out.println("HANDLER EXECUTED");
 
-        logger.info("OAuth Authentication Success Handler called");
+    logger.info("OAuth Authentication Success Handler called");
 
-        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+    DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
 
-        logger.info(user.getName());
+    logger.info(user.getName());
 
-        user.getAttributes().forEach((key,value)->{
-            logger.info("{} => {}", key, value);
-        });
+    user.getAttributes().forEach((key, value) -> {
+      logger.info("{} => {}", key, value);
+    });
 
-        logger.info(user.getAuthorities().toString());
-        
-        //data save in database
-      
+    logger.info(user.getAuthorities().toString());
 
-        new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
+    // data save in database
+
+    String email = (String) user.getAttribute("email");
+    String name = (String) user.getAttribute("name");
+    String picture = (String) user.getAttribute("picture");
+
+    // create user and save in database
+    User user1 = new User();
+    user1.setEmail(email);
+    user1.setName(name);
+    user1.setProfilePic(picture);
+    user1.setPassword("password");
+    user1.setUserId(UUID.randomUUID().toString());
+    user1.setProvider(Providers.GOOGLE);
+    user1.setEnabled(true);
+    user1.setEmailVerified(true);
+    user1.setProviderUserId(user.getName());
+    user1.setRolesList(List.of(AppConstants.ROLE_USER));
+    user1.setAbout("This account is created using google login");
+
+    User user2=userRepo.findByEmail(email).orElse(null);
+    if(user2==null){
+      userRepo.save(user1);
+      logger.info("New user created with email: {}", email);
+    }
+
+    new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
 
   }
-
-  
 
 }
